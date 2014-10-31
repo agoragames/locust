a4577d10c182d58da7e44d30fadd55fa13e11e28
Matt Wilson <mhw@hypomodern.com>
2014-10-31 15:13:50 -0400

    Whitespace

--- locust/static/locust.js
+++ locust/static/locust.js
@@ -1,53 +1,53 @@
 var shouldRefresh = false;
 
 $(window).ready(function() {
-    if($("#locust_count").length > 0) {
-        $("#locust_count").focus().select();
-    }
+  if($("#locust_count").length > 0) {
+    $("#locust_count").focus().select();
+  }
 
-    if(["hatching", "running"].indexOf($("body").attr("class")) != -1) {
-      shouldRefresh = true;
-      updateStats();
-      updateExceptions();
-    }
+  if(["hatching", "running"].indexOf($("body").attr("class")) != -1) {
+    shouldRefresh = true;
+    updateStats();
+    updateExceptions();
+  }
 
-    $("#download-requests-timeline").click(function(e) {
-      e.preventDefault();
-      downloadRPSData();
-    });
+  $("#download-requests-timeline").click(function(e) {
+    e.preventDefault();
+    downloadRPSData();
+  });
 });
 
 $("#box_stop a").click(function(event) {
-    event.preventDefault();
-    $.get($(this).attr("href"));
-    $("body").attr("class", "stopped");
-    shouldRefresh = false;
-    $(".box_stop").hide();
-    $("a.new_test").show();
-    $("a.edit_test").hide();
-    $(".user_count").hide();
+  event.preventDefault();
+  $.get($(this).attr("href"));
+  $("body").attr("class", "stopped");
+  shouldRefresh = false;
+  $(".box_stop").hide();
+  $("a.new_test").show();
+  $("a.edit_test").hide();
+  $(".user_count").hide();
 });
 
 $("#box_reset a").click(function(event) {
-    event.preventDefault();
-    $.get($(this).attr("href"));
+  event.preventDefault();
+  $.get($(this).attr("href"));
 });
 
 $("#new_test").click(function(event) {
-    event.preventDefault();
-    $("#start").show();
-    $("#locust_count").focus().select();
+  event.preventDefault();
+  $("#start").show();
+  $("#locust_count").focus().select();
 });
 
 $(".edit_test").click(function(event) {
-    event.preventDefault();
-    $("#edit").show();
-    $("#new_locust_count").focus().select();
+  event.preventDefault();
+  $("#edit").show();
+  $("#new_locust_count").focus().select();
 });
 
 $(".close_link").click(function(event) {
-    event.preventDefault();
-    $(this).parent().parent().hide();
+  event.preventDefault();
+  $(this).parent().parent().hide();
 });
 
 $("ul.tabs").tabs("div.panes > div");
@@ -57,50 +57,50 @@ var errors_tpl = $('#errors-template');
 var exceptions_tpl = $('#exceptions-template');
 
 $('#swarm_form').submit(function(event) {
-    event.preventDefault();
-    $.post($(this).attr("action"), $(this).serialize(),
-        function(response) {
-            if (response.success) {
-                $("body").attr("class", "hatching");
-                $("#start").fadeOut();
-                $("#status").fadeIn();
-                $(".box_running").fadeIn();
-                $("a.new_test").fadeOut();
-                $("a.edit_test").fadeIn();
-                $(".user_count").fadeIn();
-            }
-            shouldRefresh = true;
-            updateStats();
-            updateExceptions();
-        }
-    );
+  event.preventDefault();
+  $.post($(this).attr("action"), $(this).serialize(),
+    function(response) {
+      if (response.success) {
+        $("body").attr("class", "hatching");
+        $("#start").fadeOut();
+        $("#status").fadeIn();
+        $(".box_running").fadeIn();
+        $("a.new_test").fadeOut();
+        $("a.edit_test").fadeIn();
+        $(".user_count").fadeIn();
+      }
+      shouldRefresh = true;
+      updateStats();
+      updateExceptions();
+    }
+  );
 });
 
 $('#edit_form').submit(function(event) {
-    event.preventDefault();
-    $.post($(this).attr("action"), $(this).serialize(),
-        function(response) {
-            if (response.success) {
-                $("body").attr("class", "hatching");
-                $("#edit").fadeOut();
-            }
-        }
-    );
+  event.preventDefault();
+  $.post($(this).attr("action"), $(this).serialize(),
+    function(response) {
+      if (response.success) {
+        $("body").attr("class", "hatching");
+        $("#edit").fadeOut();
+      }
+    }
+  );
 });
 
 var sortBy = function(field, reverse, primer){
-    reverse = (reverse) ? -1 : 1;
-    return function(a,b){
-        a = a[field];
-        b = b[field];
-       if (typeof(primer) != 'undefined'){
-           a = primer(a);
-           b = primer(b);
-       }
-       if (a<b) return reverse * -1;
-       if (a>b) return reverse * 1;
-       return 0;
-    }
+  reverse = (reverse) ? -1 : 1;
+  return function(a,b){
+    a = a[field];
+    b = b[field];
+   if (typeof(primer) != 'undefined'){
+     a = primer(a);
+     b = primer(b);
+   }
+   if (a<b) return reverse * -1;
+   if (a>b) return reverse * 1;
+   return 0;
+  }
 }
 
 // Sorting by column
@@ -109,15 +109,15 @@ var desc = false;
 var report;
 var reports = [];
 $(".stats_label").click(function(event) {
-    event.preventDefault();
-    sortAttribute = $(this).attr("data-sortkey");
-    desc = !desc;
+  event.preventDefault();
+  sortAttribute = $(this).attr("data-sortkey");
+  desc = !desc;
 
-    totalRow = report.stats.pop()
-    sortedStats = (report.stats).sort(sortBy(sortAttribute, desc))
-    sortedStats.push(totalRow)
-    $('#stats tbody').jqotesub(stats_tpl, sortedStats);
-    $('#errors tbody').jqotesub(errors_tpl, (report.errors).sort(sortBy(sortAttribute, desc)));
+  totalRow = report.stats.pop()
+  sortedStats = (report.stats).sort(sortBy(sortAttribute, desc))
+  sortedStats.push(totalRow)
+  $('#stats tbody').jqotesub(stats_tpl, sortedStats);
+  $('#errors tbody').jqotesub(errors_tpl, (report.errors).sort(sortBy(sortAttribute, desc)));
 });
 
 /**
@@ -131,7 +131,7 @@ function generateChartData(input) {
   ];
   var now = new Date();
 
-  input.forEach(function( item, index) {
+  input.forEach(function( item, index ) {
     var ticksAgo = input.length - index;
     var timeMark = new Date(now - (ticksAgo * 2000)); // JS Dates are in millis, and a tick is 2 seconds.
     data[0]["data"][timeMark] = item[0];
@@ -149,7 +149,7 @@ function downloadRPSData() {
   var chart = [ [ 'Time', 'Users', 'Req/s', 'Fail Ratio' ] ];
   var now = new Date();
 
-  reports.forEach(function( item, index) {
+  reports.forEach(function( item, index ) {
     var ticksAgo = reports.length - index;
     var timeMark = new Date(now - (ticksAgo * 2000)); // JS Dates are in millis, and a tick is 2 seconds.
     chart.push([ timeMark, item[0], item[1], item[2] ]);
@@ -166,40 +166,40 @@ function downloadRPSData() {
 }
 
 function updateStats() {
-    $.get('/stats/requests', function (data) {
-        report = JSON.parse(data);
-        reports.push( extractChartData(report) );
-        $("#total_rps").html(Math.round(report.total_rps*100)/100);
-        $("#fail_ratio").html(Math.round(report.fail_ratio*100));
-        $("#status_text").html(report.state);
-        $("#userCount").html(report.user_count);
+  $.get('/stats/requests', function (data) {
+    report = JSON.parse(data);
+    reports.push( extractChartData(report) );
+    $("#total_rps").html(Math.round(report.total_rps*100)/100);
+    $("#fail_ratio").html(Math.round(report.fail_ratio*100));
+    $("#status_text").html(report.state);
+    $("#userCount").html(report.user_count);
 
-        if (report.slave_count)
-            $("#slaveCount").html(report.slave_count)
+    if (report.slave_count)
+      $("#slaveCount").html(report.slave_count)
 
-        totalRow = report.stats.pop()
-        sortedStats = (report.stats).sort(sortBy(sortAttribute, desc))
-        sortedStats.push(totalRow)
-        $('#stats tbody').jqotesub(stats_tpl, sortedStats);
-        $('#errors tbody').jqotesub(errors_tpl, (report.errors).sort(sortBy(sortAttribute, desc)));
+    totalRow = report.stats.pop()
+    sortedStats = (report.stats).sort(sortBy(sortAttribute, desc))
+    sortedStats.push(totalRow)
+    $('#stats tbody').jqotesub(stats_tpl, sortedStats);
+    $('#errors tbody').jqotesub(errors_tpl, (report.errors).sort(sortBy(sortAttribute, desc)));
 
-        // Making a charty chart chart
-        var data = generateChartData(reports);
-        new Chartkick.LineChart("charty-charty-chart", data, {
-          library: {
-            series: { 1: { targetAxisIndex: 1 } },
-            legend: { position: 'below' }
-          }
-        });
-
-        if(shouldRefresh) { setTimeout(updateStats, 2000); }
+    // Making a charty chart chart
+    var data = generateChartData(reports);
+    new Chartkick.LineChart("charty-charty-chart", data, {
+      library: {
+        series: { 1: { targetAxisIndex: 1 } },
+        legend: { position: 'below' }
+      }
     });
+
+    if(shouldRefresh) setTimeout(updateStats, 2000);
+  });
 }
 
 function updateExceptions() {
-    $.get('/exceptions', function (data) {
-        $('#exceptions tbody').empty();
-        $('#exceptions tbody').jqoteapp(exceptions_tpl, data.exceptions);
-        if(shouldRefresh) {  setTimeout(updateExceptions, 5000); }
-    });
+  $.get('/exceptions', function (data) {
+    $('#exceptions tbody').empty();
+    $('#exceptions tbody').jqoteapp(exceptions_tpl, data.exceptions);
+    if(shouldRefresh) setTimeout(updateExceptions, 5000);
+  });
 }
